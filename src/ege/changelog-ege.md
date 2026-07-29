3ae807febf9cabe4e5ba2d734a58341fa58fb7cc
Chetoh16 <egecetin1511@gmail.com>
2026-07-29 13:33:22 +0100

    docs: add changelog

---
+++ src/ege/changelog-ege.md
@@ -0,0 +1,6 @@
+# CHANGELOG
+
+## 2026/07/28
+### Added
+- Data files (`Connectome.csv, Distances.csv, Neurons_to_Muscles.csv, Sensory.csv`).
+- Created the structure for the project.
--- src/ege/load_data.py
+++ src/ege/load_data.py
@@ -8,8 +8,15 @@ DATA_DIRECTORY = Path(__file__).resolve().parent.parent / "data"
 # like -> int
 def load_connectome() -> pd.DataFrame:
     file_path = DATA_DIRECTORY / "Connectome.csv"
-    return pd.read_csv(file_path)
-    
+    df = pd.read_csv(file_path)
+    remove_unnamed_columns(df)
+    return df
+
+def remove_unnamed_columns(df) -> pd.DataFrame:    
+    new_df = df.drop(df.columns[df.columns.str.contains(
+        'Unnamed', case=False)], axis=1, inplace=True)
+    return new_df
 
 df_connectome = load_connectome()
-print(df_connectome.head())
+
+print(df_connectome)
