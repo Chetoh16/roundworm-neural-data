a7fb7b9cc3d741ee51a6139534c7228b873b34ac
Chetoh16 <egecetin1511@gmail.com>
2026-07-29 13:42:20 +0100

    feat: add all initial load functions

--- src/ege/load_data.py
+++ src/ege/load_data.py
@@ -13,6 +13,30 @@ def load_connectome() -> pd.DataFrame:
     df = pd.read_csv(file_path, index_col=0)
     return df
 
+def load_distances() -> pd.DataFrame:
+    file_path = DATA_DIRECTORY / "Distances.csv"
+    df = pd.read_csv(file_path, index_col=0)
+    return df
+
+def load_neurons_to_muscles() -> pd.DataFrame:
+    file_path = DATA_DIRECTORY / "Neurons_to_Muscles.csv"
+    df = pd.read_csv(file_path, index_col=0)
+    return df
+
+def load_sensory() -> pd.DataFrame:
+    file_path = DATA_DIRECTORY / "Sensory.csv"
+    df = pd.read_csv(file_path, index_col=0)
+    return df
+
+  
+
 df_connectome = load_connectome()
+df_distances = load_distances()
+df_neurons_to_muscles = load_neurons_to_muscles()
+df_sensory = load_sensory()
+
 
 print(df_connectome)
+print(df_distances)
+print(df_neurons_to_muscles)
+print(df_sensory)
